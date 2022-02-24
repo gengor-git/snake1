4febf271a08664c2d31448a3c794875f66b6df4d
Martin Palmowski <martin.palmowski@arvato-scs.com>
2022-02-24 16:30:28 +0100

    Yeah, change colors.

--- server.py
+++ server.py
@@ -24,10 +24,10 @@ def handle_info():
     print("INFO")
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "author": "gengor-git",  # TODO: Your Battlesnake Username
+        "color": "#800080",  # TODO: Personalize
+        "head": "#00CED1",  # TODO: Personalize
+        "tail": "#20B2AA",  # TODO: Personalize
     }
 
 
