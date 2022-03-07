a00b06ca81dafff11b33ad73f160be845128a3c8
Martin Palmowski <martin.palmowski@gmail.com>
2022-03-07 19:04:30 +0100

    Cleanup to-dos.

--- server_logic.py
+++ server_logic.py
@@ -50,7 +50,6 @@ def choose_move(data: dict) -> str:
     my_head = data["you"]["head"]  # A dictionary of x/y coordinates like {"x": 0, "y": 0}
     my_body = data["you"]["body"]  # A list of x/y coordinate dictionaries like [ {"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0} ]
 
-    # TODO: uncomment the lines below so you can see what this data looks like in your output!
     print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
     print(f"All board data this turn: {data}")
     print(f"My Battlesnakes head this turn is: {my_head}")
@@ -60,24 +59,14 @@ def choose_move(data: dict) -> str:
 
     snakes = data["board"]["snakes"]
 
-
     # Don't allow your Battlesnake to move back in on it's own neck
     #possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
 
-    # TODO: Using information from 'data', find the edges of the board and don't let your Battlesnake move beyond them
     board_height = data["board"]["height"]
     board_width = data["board"]["width"]
 
-    # TODO Using information from 'data', don't let your Battlesnake pick a move that would hit its own body
-
-    # TODO: Using information from 'data', don't let your Battlesnake pick a move that would collide with another Battlesnake
-
     # TODO: Using information from 'data', make your Battlesnake move towards a piece of food on the board
-
-    # Choose a random direction from the remaining possible_moves to move in, and then return that move
-    # move = random.choice(possible_moves)
     # TODO: Explore new strategies for picking a move that are better than random
-
 
     if not is_up_safe(my_head, my_body, board_height, board_width, snakes):
         print(f"Removing up, it's dangerous!")
@@ -94,6 +83,8 @@ def choose_move(data: dict) -> str:
 
     # move = possible_moves[0] # random.choice(possible_moves)
 
+
+    # TODO: Select the smartest move from the possible moves list.
     move = possible_moves[-1]
 
     print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
