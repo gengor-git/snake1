aaa2a64d43a6b541bcab9906b26abc1a26f645f4
Martin Palmowski <martin.palmowski@gmail.com>
2022-03-07 19:04:30 +0100

    Cleanup to-dos.

--- server_logic.py
+++ server_logic.py
@@ -60,24 +60,14 @@ def choose_move(data: dict) -> str:
 
     snakes = data["board"]["snakes"]
 
-
     # Don't allow your Battlesnake to move back in on it's own neck
     #possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
 
-    # TODO: Using information from 'data', find the edges of the board and don't let your Battlesnake move beyond them
     board_height = data["board"]["height"]
     board_width = data["board"]["width"]
 
-    # TODO Using information from 'data', don't let your Battlesnake pick a move that would hit its own body
-
-    # TODO: Using information from 'data', don't let your Battlesnake pick a move that would collide with another Battlesnake
-
     # TODO: Using information from 'data', make your Battlesnake move towards a piece of food on the board
-
-    # Choose a random direction from the remaining possible_moves to move in, and then return that move
-    # move = random.choice(possible_moves)
     # TODO: Explore new strategies for picking a move that are better than random
-
 
     if not is_up_safe(my_head, my_body, board_height, board_width, snakes):
         print(f"Removing up, it's dangerous!")
