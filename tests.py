b477987565d514e076736e24048a31b7013eec21
Martin Palmowski <martin.palmowski@gmail.com>
2022-03-08 12:44:38 +0100

    Added first test.

--- tests.py
+++ tests.py
@@ -11,6 +11,82 @@ in the folder where this file exists:
 
 """
 import unittest
+from server_logic import avoid_snakes
+
+class AvoidSnakesTest(unittest.TestCase):
+    def test_avoid_snakes_up(self):
+        my_head = {"x": 2, "y": 3}
+        result_moves = {
+            "up": {
+                "x": my_head["x"], 
+                "y": my_head["y"] + 1
+            }
+        }
+        possible_moves = {
+            "up": {
+                "x": my_head["x"], 
+                "y": my_head["y"] + 1
+            }, 
+            "down": {
+                "x": my_head["x"], 
+                "y": my_head["y"] - 1
+            }, 
+            "left": {
+                "x": my_head["x"] - 1, 
+                "y": my_head["y"]
+            }, 
+            "right": {
+                "x": my_head["x"] + 1, 
+                "y": my_head["y"]
+            }, 
+        }
+        snakes = [
+            {
+            "id": "test1",
+            "name": "Test Snake 1",
+            "health": 54,
+            "body": [
+                {"x": 2, "y": 3}, 
+                {"x": 1, "y": 3}, 
+                {"x": 0, "y": 3}
+            ],
+            "latency": "123",
+            "head": {"x": 0, "y": 0},
+            "length": 3,
+            "shout": "why are we shouting??",
+            "squad": "1",
+            "customizations":{
+                        "color":"#26CF04",
+                        "head":"smile",
+                        "tail":"bolt"
+                        }
+            },
+            {
+            "id": "test2",
+            "name": "Test Snake 2",
+            "health": 54,
+            "body": [
+                {"x": 3, "y": 3}, 
+                {"x": 3, "y": 2}, 
+                {"x": 2, "y": 2}
+            ],
+            "latency": "123",
+            "head": {"x": 0, "y": 0},
+            "length": 3,
+            "shout": "why are we shouting??",
+            "squad": "1",
+            "customizations":{
+                        "color":"#26CF04",
+                        "head":"smile",
+                        "tail":"bolt"
+                        }
+            },
+
+        ]
+
+        possible_moves = avoid_snakes(snakes, possible_moves)
+        self.assertEqual(len(possible_moves), 1)
+        self.assertDictEqual(result_moves, possible_moves)
 
 if __name__ == "__main__":
     unittest.main()
